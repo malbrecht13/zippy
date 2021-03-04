e0343b01331148dbe26e53ddd9fc8dc9b06a538e
malbrecht13 <44516083+malbrecht13@users.noreply.github.com>
2021-03-04 03:47:07 -0600

    Able to display vehicles

--- model/vehicles_db.php
+++ model/vehicles_db.php
@@ -4,7 +4,14 @@
 
     function get_vehicles() {
         global $db;
-        $query = 'SELECT * FROM vehicles';
+        $query = 'SELECT v.year, v.model, v.price, t.type, c.class, m.make FROM vehicles v
+                  INNER JOIN classes c 
+                  ON c.class_id = v.class_id
+                  INNER JOIN types t
+                  ON v.type_id = t.type_id
+                  INNER JOIN makes m
+                  ON m.make_id = v.make_id
+                  ORDER BY v.price DESC';
         $statement = $db->prepare($query);
         $statement->execute();
         $vehicles = $statement->fetchAll();
--- public_view/vehicle_list.php
+++ public_view/vehicle_list.php
@@ -1,3 +1,47 @@
 <?php include('header.php'); ?>
 
+<select class="auto_selector">
+    <option value="">View All Makes</option>
+</select>
+<select class="auto_selector">
+    <option value="">View All Types</option>
+</select>
+<select class="auto_selector">
+    <option value="">View All Classes</option>
+</select>
+<div class="container-fluid sortBy">
+    <p class="sortBy__label">Sort by</label>
+    <form action="." method="POST" class="sortBy__form">
+        <input class="sortBy__form_radio" type="radio" name="sort" id="sort_by_price">
+        <label for="sort_by_price">Price</label>
+        <input class="sortBy__form_radio" type="radio" name="sort" id="sort_by_year">
+        <label for="sort_by_year">Year</label>
+    <button class="btn btn-primary" type="submit">Submit</button>
+    </form>
+</div>
+<table>
+    <thead>
+        <tr>
+            <th>Year</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Type</th>
+            <th>Class</th>
+            <th>Price</th>
+        </tr>
+        <?php foreach($vehicles as $vehicle) : ?>
+            <tr>
+                <td><?= $vehicle['year'] ?></td>
+                <td><?= $vehicle['make'] ?></td>
+                <td><?= $vehicle['model'] ?></td>
+                <td><?= $vehicle['type'] ?></td>
+                <td><?= $vehicle['class'] ?></td>
+                <td><?= $vehicle['price'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </thead>
+
+</table>
+
+
 <?php include('footer.php'); ?>
