f32fc0cb895ac9fa63917d345ad233c28b59403f
malbrecht13 <44516083+malbrecht13@users.noreply.github.com>
2021-03-05 23:00:01 -0600

    Try to connect to database

--- model/database.php
+++ model/database.php
@@ -2,7 +2,7 @@
 
     $host = 'pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
     $dbname = 'gvdsr24jhvu8j757';
-    $dsn = "mysql:host=$host;dbname=$dbname";
+    $dsn = `mysql:host=$host;dbname=$dbname`;
     $username = 'dzq33wn2lmpzso31';
     $password = 'l3pzfll323cekhd8';
 
